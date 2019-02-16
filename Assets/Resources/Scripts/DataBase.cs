d302453ae97cf495852f19eb1013da1888b3a668
3D Magic VR <34051446+3DMagicVR@users.noreply.github.com>
2019-02-16 10:30:16 -0600

    Algunos objetos removidos
    Se removieron algunos objetos no utilizados.

---
+++ Assets/Resources/Scripts/DataBase.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager
+{
+    int valueA, valueB;
+    List<int> values = new List<int>();
+    Vector3 positionNew;
+    MonoBehaviour component;
+
+    public Manager(int valueA, int valueB)
+    {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        positionNew = component.gameObject.transform.position;
+    }
+
+    public int ValueA
+    {
+        get;
+        private set;
+    }
+
+    public int ValueB
+    {
+        get
+        {
+            return valueB;
+        }
+    }
+}
+
+public class DataBase : MonoBehaviour
+{
+    public static DataBase dataBase;
+    public Manager manager;
+
+    private void Awake()
+    {
+        manager = new Manager(1, 2);
+
+        if (dataBase == null)
+        {
+            dataBase = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogFormat("Current values on manager: {0} - {1}", manager.ValueA, manager.ValueB);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
